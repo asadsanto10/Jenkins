c28fc96f9b730c732fdeb8143394e8e6ce3ef692
asaduzzaman santo <asaduzzaman.santo@technonext.com>
2026-02-19 12:07:36 +0600

    change helath route msg

--- app/server.js
+++ app/server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     );
   } else if (req.url === "/health" && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ status: "healthy1111" }));
+    res.end(JSON.stringify({ status: "healthy asdsad asd asdsa" }));
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Not Found" }));
